a30002f39387c29cfd46f7ad3edb4de2c5914fa7
Bhavani Priya <bhavanipriya169@gmail.com>
2025-01-15 18:37:56 -0500

    VestaEquity_LLM
    Utilized LLMs to create a QA for Vesta Equity's most commonly asked questions

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
